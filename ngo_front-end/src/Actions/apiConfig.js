7533788205685dd8d0104495bb97dd502cc04cbf
[firstname <223264883+saloniwarudkar0@users.noreply.github.com>
2026-09-17 16:52:08 +0530

    Connect frontend to Render backend

--- ngo_front-end/src/Actions/apiConfig.js
+++ ngo_front-end/src/Actions/apiConfig.js
@@ -1,4 +1,4 @@
-const productionApiUrl = 'https://backend-jalsanrakshanam.thembs.in'
+const productionApiUrl = 'https://ngo-backend-roq2.onrender.com'
 const localApiUrl = 'http://127.0.0.1:5001'
 
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL
