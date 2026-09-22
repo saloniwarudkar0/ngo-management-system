9de98387bda8a104deb4d3f5f6b0609b7009a711
[firstname <223264883+saloniwarudkar0@users.noreply.github.com>
2026-09-22 18:47:52 +0530

    Update contact email

--- ngo_front-end/src/Config.js
+++ ngo_front-end/src/Config.js
@@ -1,7 +1,7 @@
 export const config ={
         'universal_references':{
             'phone_number': 9834498615,
-            'email' : 'Harishpaliwal07@gmail.com',
+            'email' : 'harishpaliwal07@gmail.com',
             'instagram' : 'https://instagram.com',
             'facebook' : 'https://facebook.com',
             'twitter' : 'https://x.com',
